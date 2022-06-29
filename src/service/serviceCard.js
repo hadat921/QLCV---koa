2d5b87b8437a1d2d40cc9417ed0ea0fe8df7d629
GiaDat <hagiadat0988968570@gmail.com>
2022-06-29 16:10:05 +0700

    update service

--- src/service/serviceCard.js
+++ src/service/serviceCard.js
@@ -24,24 +24,29 @@ const serviceCard = async (ctx) => {
     if (createdAtFrom && createdAtTo) {
         const from = moment(createdAtFrom).startOf('day').format("YYYY-MM-DD HH:mm:ss")
         const to = moment(createdAtTo).endOf('day').format("YYYY-MM-DD HH:mm:ss")
-
         condition.createdAt = {
             [Op.between]: [from, to]
         }
+        return condition;
+
     }
     if (createdAtFrom) {
+
         const from = moment(createdAtFrom).startOf('day').format("YYYY-MM-DD HH:mm:ss")
 
         condition.createdAt = {
             [Op.gte]: from
         }
+        return condition;
     }
     if (createdAtTo) {
+
         const to = moment(createdAtTo).endOf('day').format("YYYY-MM-DD HH:mm:ss")
 
         condition.createdAt = {
             [Op.lte]: to
         }
+        return condition;
     }
     return condition;
 
